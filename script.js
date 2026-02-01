d2b2959d0cd9640a0dc5133466f4faef7b333210
nolasco-daniel <dn8000375@gmail.com>
2026-02-01 22:17:48 +0800

    elows

--- script.js
+++ script.js
@@ -80,6 +80,6 @@ class UIController {
 }
 
 // START APP
-const invite = new Invitation("Ayheca", "HO!HO!HO!HO!");
+const invite = new Invitation("Ayheca", "HO!HO!HO!HO!HO!");
 const ui = new UIController(invite);
 ui.render();
